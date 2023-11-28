426c49961ce383700bbe87944f2d180f6ca71c95
Wang <1826967276@qq.com>
2023-11-28 22:04:06 +0800

    Site updated: 2023-11-28 22:04:05

--- js/custom/foot.js
+++ js/custom/foot.js
@@ -24,5 +24,5 @@ hrsold=Math.floor(e_hrsold);
 e_minsold=(e_hrsold-hrsold)*60;
 minsold=Math.floor((e_hrsold-hrsold)*60);
 seconds=Math.floor((e_minsold-minsold)*60);
-span_dt_dt.innerHTML='<font style=color:#fff>'+daysold+'</font> 天 <font style=color:#fff>'+hrsold+'</font> 时 <font style=color:#fdb933>'+minsold+'</font> 分 <font style=color:#fff>'+seconds+'</font> 秒';
+span_dt_dt.innerHTML='<font style=color:#fff>'+daysold+'</font> 天 <font style=color:#fff>'+hrsold+'</font> 时 <font style=color:#fff>'+minsold+'</font> 分 <font style=color:#fff>'+seconds+'</font> 秒';
 }
